d287c4603104c4e8f347a91b57d9baff6ec4b9a2
VerasNp <vitorveras01@gmail.com>
2025-02-13 12:51:50 -0300

    Adds DoxyFile

--- MWP/include/Matrix.hpp
+++ MWP/include/Matrix.hpp
@@ -2,7 +2,6 @@
 
 #include "Vector.hpp"
 #include <cmath>
-#include <iostream>
 #include <stdexcept>
 #include <utility>
 #include <vector>
@@ -20,8 +19,7 @@ public:
   /**
    * @brief Default constructor for matrix
    *
-   * Init a empty matrix and size equal to zero
-   *
+   * Initializes an empty matrix with size equal to zero (0x0)
    */
   Matrix<T>();
 
@@ -29,7 +27,7 @@ public:
    * @brief Constructor for the Matrix class.
    *
    * This constructor initializes the matrix with the given number of rows and
-   * columns and set all elements to zero.
+   * columns, and sets all elements to zero.
    *
    * @tparam T The type of the matrix elements.
    * @param rows The number of rows in the matrix.
